1d20d14535234828168a9cbec7c6dc1a997ca047
oohashitakayuki <ssg675vl@docomo.ne.jp>
2026-04-07 17:13:21 +0900

    storeメソッドとdestroyメソッドを作成

--- share-backend/app/Http/Controllers/LikeController.php
+++ share-backend/app/Http/Controllers/LikeController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LikeController extends Controller
 {
-    //
+    public function store($postId)
+    {
+        $user = Auth::user();
+
+        if (!$user->is_like($postId)) {
+            $user->likes()->create([
+                'post_id' => $postId,
+            ]);
+        }
+
+        return response()->json(['message' => 'liked']);
+    }
+
+    public function destroy($postId)
+    {
+        $user = Auth::user();
+
+        $like = Like::where('post_id', $postId)
+            ->where('user_id', $user->id)
+            ->first();
+
+        if($like) {
+            $like->delete();
+        }
+
+        return response()->json(['message' => 'unliked']);
+    }
 }
