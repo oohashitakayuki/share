7bdbea8880333b1495a11287e500abe6e25ea547
oohashitakayuki <ssg675vl@docomo.ne.jp>
2026-04-15 14:34:30 +0900

    リレーションを設定

--- share-backend/app/Models/User.php
+++ share-backend/app/Models/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Like::class);
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
