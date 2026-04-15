81b190fe75b372da6d614b21e5a8d8e6893ab428
oohashitakayuki <ssg675vl@docomo.ne.jp>
2026-04-15 11:10:10 +0900

    ビューの修正

--- share-frontend/pages/posts/_id.vue
+++ share-frontend/pages/posts/_id.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
     <p>コメント</p>
+
+    <Message
+      v-if="message"
+      :message="message"
+      :showDetailButton="false"
+    />
   </div>
 </template>
+
+<script>
+import Message from '~/components/Message.vue'
+
+export default {
+  middleware:'auth',
+
+  components: {
+    Message
+  },
+
+  data() {
+    return {
+      message: null
+    }
+  },
+
+  async asyncData({ $axios, params }) {
+    const res = await $axios.get(`/api/posts/${params.id}`)
+    return {
+      message: res.data
+    }
+  }
+}
+</script>
