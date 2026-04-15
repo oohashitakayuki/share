d5b2890bd51120b35f96db7733b6b8f4ed74d12d
oohashitakayuki <ssg675vl@docomo.ne.jp>
2026-04-15 14:33:08 +0900

    storeメソッドを作成

--- share-backend/app/Http/Controllers/CommentController.php
+++ share-backend/app/Http/Controllers/CommentController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+
+        $comment = Comment::create([
+            'user_id' => $user->id,
+            'post_id' => $request->post_id,
+            'comment' => $request->comment,
+        ]);
+
+        // ユーザー情報も返す
+        $comment->load('user');
+
+        return response()->json($comment);
+    }
 }
