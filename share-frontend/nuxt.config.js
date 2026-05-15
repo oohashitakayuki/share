88129846d90692941f862adf26720a038ffe57a9
oohashitakayuki <ssg675vl@docomo.ne.jp>
2026-05-15 17:16:53 +0900

    CSSファイルを指定

--- share-frontend/nuxt.config.js
+++ share-frontend/nuxt.config.js
@@ -24,6 +24,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/css/sanitize.css',
+    '~/assets/css/common.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
