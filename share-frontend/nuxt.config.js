955b5628628e868863f522161833baa9afc2a1d0
oohashitakayuki <ssg675vl@docomo.ne.jp>
2026-05-27 16:46:41 +0900

    titleをshareに変更

--- share-frontend/nuxt.config.js
+++ share-frontend/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'share-frontend',
+    title: 'share',
     htmlAttrs: {
       lang: 'en'
     },
